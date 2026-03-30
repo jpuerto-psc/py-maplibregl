6ce53901950b55f988bd7de85eb7f4e75c1e4d56
Juan Puerto <=>
2026-03-30 11:22:28 -0400

    feat: update bindings

--- get-bindings.sh
+++ get-bindings.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-branch=${1:-dev}
+branch=${1:-main}
 echo $branch
 target_dir=maplibre/srcjs
 echo $target_dir
-curl https://raw.githubusercontent.com/eoda-dev/maplibre-bindings/${branch}/py-bindings/pymaplibre.js -o $target_dir/pywidget.js
-curl https://raw.githubusercontent.com/eoda-dev/maplibre-bindings/${branch}/py-bindings/pymaplibre.css -o $target_dir/pywidget.css
+curl https://raw.githubusercontent.com/jpuerto-psc/maplibre-bindings/${branch}/py-bindings/pymaplibre.js -o $target_dir/pywidget.js
+curl https://raw.githubusercontent.com/jpuerto-psc/maplibre-bindings/${branch}/py-bindings/pymaplibre.css -o $target_dir/pywidget.css
 # curl https://raw.githubusercontent.com/eoda-dev/maplibre-bindings/${branch}/py-bindings/pymaplibre.ipywidget.js -o $target_dir/ipywidget.js
 # curl https://raw.githubusercontent.com/eoda-dev/maplibre-bindings/${branch}/py-bindings/pymaplibre.ipywidget.css -o $target_dir/ipywidget.css
